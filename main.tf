430f5c10f5129b820c1aaabde2af9ab2e4ad00e3
catchmeraman <catchmeraman@gmail.com>
2025-10-28 12:29:34 +0530

    Fix duplicate provider configuration

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+# Provider configurations moved to versions.tf
 
 provider "aws" {
   alias  = "us_east_1"
